f227f4d67681ee3e71edbfb8619cbe9c1e49f2d8
Daniel S. Waite <daniel.s.waite@aexp.com>
2023-03-02 16:40:35 -0700

    remove falling test and build

--- src/App.test.js
+++ src/App.test.js
@@ -11,7 +11,3 @@ it('should render', () => {
 
   expect(screen.getByText('Business Contacts')).toBeInTheDocument();
 });
-
-it('should do something amazing', () => {
-  expect(true).toBe(false);
-});
