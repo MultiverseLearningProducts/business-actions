540e1e577d16a684e4c9fcc4edba676ecddc1bfc
Krystal <lopezkrystal10@gmail.com>
2023-03-30 13:52:48 -0700

    added failing test

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,17 @@
 /** @jest-environment jsdom */
-const {fetch} = require('whatwg-fetch');
+const { fetch } = require("whatwg-fetch");
 global.fetch = fetch;
-import React from 'react';
-import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
-import { App } from './App';
+import React from "react";
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import { App } from "./App";
 
-it('should render', () => {
+it("should render", () => {
   render(<App />);
 
-  expect(screen.getByText('Business Contacts')).toBeInTheDocument();
+  expect(screen.getByText("Business Contacts")).toBeInTheDocument();
+});
+
+it("should do something amazing", () => {
+  expect(true).toBe(false);
 });
