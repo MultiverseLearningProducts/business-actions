a30f6d791b0dde530f5623ff799aaa5d2543f15b
404-lorena <lramirezlopez@google.com>
2023-04-26 15:19:00 -0400

    Add a failing test

--- src/App.test.js
+++ src/App.test.js
@@ -11,3 +11,7 @@ it('should render', () => {
 
   expect(screen.getByText('Business Contacts')).toBeInTheDocument();
 });
+
+it('should do something amazing', () => {
+  expect(true).toBe(false);
+});
