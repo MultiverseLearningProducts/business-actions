df6e92c1e4ab6207f1deb1b74106cbfcc7f34d70
Rachel Boursia <91563448+rachelboursia@users.noreply.github.com>
2023-07-11 14:30:50 -0700

    removes failing test

--- src/App.test.js
+++ src/App.test.js
@@ -12,6 +12,3 @@ it('should render', () => {
   expect(screen.getByText('Business Contacts')).toBeInTheDocument();
 });
 
-it('should do something amazing', () => {
-  expect(true).toBe(false);
-});
